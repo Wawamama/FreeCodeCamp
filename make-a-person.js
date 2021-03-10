e32208cdbdd5c47ec23fad880ec26594beb40b95
Marion Saul <77323113+Wawamama@users.noreply.github.com>
2021-03-10 20:18:03 +0100

    Add files via upload

---
+++ make-a-person.js
@@ -0,0 +1,46 @@
+var Person = function(firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  this.getFullName = function() {
+      if (!this.firstName && !this.lastName) { 
+        return firstAndLast.split(' ').join(' ');
+      }
+      if (this.firstName && this.lastName) {
+        return this.firstName + ' ' + this.lastName;
+      }
+      if (this.firstName && !this.lastName) {
+        return this.firstName + ' ' + firstAndLast.split(' ')[1]
+      }
+      if (!this.firstName && this.lastName) {
+        return 'Haskell ' + this.lastName;
+      }
+
+  };
+
+  this.getFirstName = function() {
+    if (this.firstName) return this.firstName;
+    return firstAndLast.split(' ')[0];
+  }
+
+  this.getLastName = function() {
+    if (this.lastName) return this.lastName;
+    return firstAndLast.split(' ')[1];
+  }
+
+  this.setFirstName = function(first) {
+      this.firstName = first;
+  }
+  this.setLastName = function(last) {
+      this.lastName = last;
+  }
+  this.setFullName = function(firstAndLast) {
+      this.firstName = firstAndLast.split(' ')[0];
+      this.lastName = firstAndLast.split(' ')[1];
+  }
+
+};
+
+var bob = new Person('Bob Ross');
+//bob.setFirstName("Haskell");
+//bob.setLastName("Curry");
+console.log(bob.getFullName())
